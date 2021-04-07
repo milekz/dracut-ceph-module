5187771d2a2f4e21f9aaf4fc27b0cda0a2a6be20
Miłosz Szewczak <milek@milek.eu>
2021-04-07 14:24:43 +0200

    Update rbdroot.sh

--- usr/lib/dracut/modules.d/95rbd/rbdroot.sh
+++ usr/lib/dracut/modules.d/95rbd/rbdroot.sh
@@ -1,6 +1,34 @@
 #!/bin/sh
 
-. /lib/rbd-lib.sh
+#. /lib/rbd-lib.sh
+
+
+ceph_to_var() {
+    local cephuser; local cephpass
+    # Check required arguments
+    server=${1##ceph://}
+    cephuser=${server%@*}
+    cephpass=${cephuser#*:}
+    if [ "$cephpass" != "$cephuser" ]; then
+	cephuser=${cephuser%:*}
+    else
+	cephpass=$(getarg cephpass)
+    fi
+    if [ "$cephuser" != "$server" ]; then
+	server="${server#*@}"
+    else
+	cephuser=$(getarg cephuser)
+    fi
+
+    path=${server#*/}
+    server=${server%:/*}
+
+    if [ ! "$cephuser" -o ! "$cephpass" ]; then
+	die "For CEPH support you need to specify a cephuser and cephpass either in the cephuser and cephpass commandline parameters or in the root= CEPH URL."
+    fi
+    options="name=$cephuser,secret=$cephpass,noatime,nodiratime"
+}
+
 
 [ "$#" = 3 ] || exit 1
 
@@ -19,8 +47,11 @@ echo rbd: $pool/$name
 
 # Attempt to map the rbd device.
 echo "$server $options $pool $name" > /sys/bus/rbd/add
-
 mount /dev/rbd0 $NEWROOT -o noatime,nodiratime && { [ -e /dev/root ] || ln -s null /dev/root ; }
+
+echo "$server $options $pool $name" > /sys/bus/rbd/add_single_major
+mount /dev/rbd0 $NEWROOT -o noatime,nodiratime && { [ -e /dev/root ] || ln -s null /dev/root ; }
+
 
 # inject new exit_if_exists
 echo 'settle_exit_if_exists="--exit-if-exists=/dev/root"; rm -f -- "$job"' > $hookdir/initqueue/ceph.sh
